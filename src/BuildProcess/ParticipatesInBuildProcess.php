f3e35018eeaa402cf31a9a0b7a88cd50ed6a9377
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-11 20:33:37 -0600

    Merging vapor-cli-module to master

---
+++ src/BuildProcess/ParticipatesInBuildProcess.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Laravel\VaporCli\BuildProcess;
+
+use Illuminate\Filesystem\Filesystem;
+use Laravel\VaporCli\Path;
+
+trait ParticipatesInBuildProcess
+{
+    protected $environment;
+    protected $appPath;
+    protected $vendorPath;
+    protected $path;
+    protected $vaporPath;
+    protected $buildPath;
+    protected $files;
+    protected $environmentType;
+
+    /**
+     * Create a new project builder.
+     *
+     * @param string|null $environment
+     *
+     * @return void
+     */
+    public function __construct($environment = null, $environmentType = null)
+    {
+        $this->environment = $environment;
+
+        $this->appPath = Path::app();
+        $this->vendorPath = Path::vendor();
+        $this->path = Path::current();
+        $this->vaporPath = Path::vapor();
+        $this->buildPath = Path::build();
+
+        $this->files = new Filesystem();
+        $this->environmentType = $environmentType;
+    }
+}
